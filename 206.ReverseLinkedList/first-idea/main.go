85357ab7d1a0b7376faf0e37356a3bc7acfafe89
a.pashko <pashko.anton@gmail.com>
2023-06-08 00:29:35 +0300

    Array&Hashing|neetcode|206.ReverseLinkedList

---
+++ 206.ReverseLinkedList/first-idea/main.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	ReverseList(&ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	})
+}
+
+func ReverseList(head *ListNode) *ListNode {
+	var linkedList *ListNode
+	for head != nil {
+		linkedList = &ListNode{
+			Val:  head.Val,
+			Next: linkedList,
+		}
+		head = head.Next
+	}
+
+	return linkedList
+}
---
+++ 206.ReverseLinkedList/optimizations/go.mod
@@ -0,0 +1,3 @@
+module reverselinkedlistv2
+
+go 1.20
